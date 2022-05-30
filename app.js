89afd187a6230a71a1712b253f275a7d2d9881b1
Saikumar Aleti <kumarchintu70880@gamil.com>
2022-05-30 15:05:50 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+
+//async errors
+require("express-async-errors");
+
+const express = require("express");
+
+const app = express();
+
+const productsRouter = require("./routes/products");
+
+const notFoundMiddleware = require("./middleware/not-found");
+
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+const connectDB = require("./db/connect");
+
+//express middleware
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+  res.send(`<h1>Store API</h1><a href="/api/v1/products">Products</a>`);
+});
+app.use("/api/v1/products", productsRouter);
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {}
+};
+
+start();
---
+++ controllers/products.js
@@ -0,0 +1,10 @@
+const getAllProductsStatic = async (req, res) => {
+  throw new Error("Testing express-async errors");
+  res.status(200).json({ msg: `Products testing route` });
+};
+
+const getAllProducts = async (req, res) => {
+  res.status(200).json({ msg: `Products route` });
+};
+
+module.exports = { getAllProductsStatic, getAllProducts };
